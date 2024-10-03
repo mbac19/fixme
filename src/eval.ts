9a7f5b4a237f1f0c7e9bc3a69f44c326f80872c2
Brendan McNamara <gh@brendan9.com>
2024-10-03 11:40:29 -0700

    Fix type error

--- src/eval.ts
+++ src/eval.ts
@@ -1,13 +1,15 @@
-import { Operator } from "./operator";
+import { MathASTNode } from "./math_ast";
+import { Parser } from "./parser";
 
 export type ResolvedSymbols = Record<string, number>;
 
 export const Eval = {
   eval(text: string, symbols?: ResolvedSymbols): number {
-    throw Error("TODO");
+    const parsed = Parser.parse(text);
+    return Eval.evalNode(parsed, symbols);
   },
 
-  evalOperators(operator: Operator, symbols?: ResolvedSymbols): number {
+  evalNode(operator: MathASTNode, symbols?: ResolvedSymbols): number {
     throw Error("TODO");
   },
 };
