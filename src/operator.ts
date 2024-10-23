152592adb5fd60c90a8401288690caa87efd5e93
Brendan McNamara <gh@brendan9.com>
2024-10-23 12:30:03 -0700

    Tiny bit of doc

--- src/operator.ts
+++ src/operator.ts
@@ -4,6 +4,16 @@ export enum OperatorType {
   Function,
 }
 
+/**
+ * When enountering operators (typically infix binary operators), the order
+ * that we execute the operators is ambiguous. For example (2 + 3 * 7) has
+ * ambiguity as to whether we should add 2 and 3 first, or multiply 3 and 7
+ * first. This enum is used to resolve that ambiguity by defining the
+ * precedence that we should handle operators.
+ *
+ * If 2 operators have the same precedence, then we choose the operator that
+ * shows up first when reading left to right.
+ */
 export enum OperatorPrecedence {
   Low,
   Normal,
